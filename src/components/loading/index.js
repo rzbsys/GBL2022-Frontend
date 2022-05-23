0614f0c912e12bc9336c503f2f5c59ff4ea20de3
rzbsys <20-10520@dshs.kr>
2022-05-24 01:01:54 +0900

    ADD : 로딩 컴포넌트 생성

---
+++ src/components/loading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+function Loading() {
+    return (
+        <div className='Loading'>
+            <h1>로딩중</h1>
+        </div>
+    );
+}
+
+export default Loading;
